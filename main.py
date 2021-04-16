70641e663f655c4849b1932033e41393cc2b9372
George Mihăilă <george_mihaila@outlook.com>
2021-04-16 08:57:47 +0300

    Add rate limiting and data dumping

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from stravalib.client import Client
 from requests import request
 import time
 import configparser
+import csv
+import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -12,19 +14,22 @@ client.client_id = credentials['client_id']
 client.access_token = credentials['access_token']
 client.refresh_token = credentials['refresh_token']
 client.code = credentials['code']
-print(client.access_token)
 
-athlete = client.get_athlete()
-
-print(athlete)
+all_data = []
 for activity in client.get_activities():
-    print(activity)
-    print(client.get_activity(activity.id))
-    types = [ 'time', 'latlng', 'distance', 'altitude', 'velocity_smooth',
-    'heartrate', 'cadence', 'watts', 'temp', 'moving', 'grade_smooth' ]
+    data = {}
+    data['activity'] = str(activity)
+    types = config['data']['import'].split(',')
     streams = client.get_activity_streams(activity.id, types=types)
     for t in types:
         if t in streams.keys():
-            print(t + " - ")
-            print(streams[t].data)
-    break
+            data[t] = streams[t].data
+    print('Imported {}'.format(activity.id))
+    all_data.append(data)
+
+    time.sleep(9 * 2) #Rate limited to 100 requests every 15 minutes (1 request / 9 seconds)
+
+with open('all_data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+print('Wrote data to all_data.json')
